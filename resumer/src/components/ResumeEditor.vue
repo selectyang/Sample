e7286a6c97527a0e5f8c8f43d539a0dc9be1417e
yanjiawen <yangjiawen_01@163.com>
2017-07-17 23:46:43 +0800

    add RseumeEditor content

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -13,7 +13,19 @@
       </nav>
       <ol class="panels">
           <li v-for="item in resume.config" v-show="item.field === selected">
-              {{resume[item.field]}}
+              <div v-if="resume[item.field] instanceof Array">
+                <div class="subitem" v-for="subitem in resume[item.field]">
+                  <div class="resumeField" v-for="(value,key) in subitem">
+                    <label >{{key}}</label>
+                    <input type="text" :value="value"/>
+                  </div>
+                  <hr/>
+                </div>
+              </div>
+              <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+                <label >{{key}}</label>
+                <input type="text" v-model="resume[item.field][key]"/>
+             </div>
           </li>
       </ol>
   </div>
@@ -40,24 +52,50 @@ export default {
           city: '',
           title: ''
         },
-        'work history': [],
-        education: [],
-        projects: [],
-        awards: [],
-        contacts: [],
+        'work history': [
+          {company: 'AL',content: '这是我的第二份工作'},
+          {company: 'TX',contenr: '这是我的第一份工作'}
+        ],
+        education: [
+          {school: 'A',content: '文字'},
+          {school: 'B',content: '文字'},
+        ],
+        projects: [
+          {name: 'project A',content: '文字'},
+          {name: 'porject B',content: '文字'},
+        ],
+        awards: [
+          {name: 'awards A',content: '文字'},
+          {name: 'awards B',content: '文字'},
+        ],
+        contacts: [
+          {contacts: 'phone',content: '777777911'},
+          {name: 'QQ',content: '3838998'},
+        ],
       }
     }
   }
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 #resumeEditor{
     background: #fff;
     box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
     display: flex;
     flex-direction: row;
     overflow: auto;
+    > .panels {
+        flex-grow: 1;
+        > li{
+            padding: 24px;
+            hr {
+                border: none;
+                border-top: 1px solid #ddd;
+                margin: 24px 0;
+            }
+        }
+    }
     > nav {
         width: 80px;
         background: black;
@@ -86,5 +124,20 @@ svg.icon{
 ol {
     list-style: none;
 }
+.resumeField {
+    > label {
+        display: block;
+    }
+    > input[type=text]{
+        margin: 16px 0;
+        border: 1px solid #ccc;
+        box-shadow: inset 0 1px 3px 0 rgba(0,0,0,.25);
+        width: 100%;
+        height: 40px;
+        padding: 16px 0;
+    }
+}
+
+
 </style>
 
