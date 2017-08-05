ac559bfb8b1be3824f9263e454c1b0f29b911a2d
yanjiawen <yangjiawen_01@163.com>
2017-08-05 23:38:49 +0800

    update ResumeEditor 样式

--- resumer_v2/src/components/ResumeEditor.vue
+++ resumer_v2/src/components/ResumeEditor.vue
@@ -8,6 +8,7 @@
                     <svg  class="icon" aria-hidden="true">
                         <use :xlink:href="`#icon-${item.icon}`"></use>
                     </svg>
+                    <div class="gk_gl"></div>
                 </li>
             </ol>
             <div class="seting">
@@ -17,7 +18,22 @@
             </div>
         </nav>
         <ol class="panels">
-            <li v-for="item in resume.visibleItems" v-show="item === selected">{{resume[item]}}</li>
+            <li v-for="item in resume.config" v-show="item.field === selected">
+                <h3>{{item.field}}</h3>
+                <div v-if="resume[item.field] instanceof Array">
+                    <div class="subitem" v-for="subitem in resume[item.field]">
+                        <div class="resumeField" v-for="(value,key) in subitem">
+                            <lable>{{key}}</lable>
+                            <input type="text" :value="value"/>
+                        </div>
+                        <hr/>
+                    </div>
+                </div>
+                <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+                    <lable>{{key}}</lable>
+                    <input type="text" v-model="resume[item.field][key]"/>
+                </div>
+            </li>
         </ol>
     </div>
 </template>
@@ -27,9 +43,8 @@
       name: 'ResumeEidtor',
       data() {
         return {
-          selected: 'bio',
+          selected: 'profile',
           resume: {
-            visibleItems: ['profile','workHistory','education','projects','awards','contacts'],
             config: [
                 {field: 'profile', icon: 'mingpian'},
                 {field: 'workHistory', icon: 'work'},
@@ -43,26 +58,42 @@
               city: '',
               title: ''
             },
-            workHistory: [],
-            education: [],
-            prijects: [],
-            awards: [],
-            contacts: []
+            workHistory: [
+              {company: 'AL', content: '我的第二份工作是'},
+              {company: 'TX', content: '我的第一份工作是'},
+            ],
+            education: [
+              {school: 'AL', content: '文字'},
+              {school: 'TX', content: '文字'},
+            ],
+            projects: [
+              {name: 'project A', content: '文字'},
+              {name: 'project B', content: '文字'},
+            ],
+            awards: [
+              {name: 'awards A', content: '文字'},
+              {name: 'awards B', content: '文字'},
+            ],
+            contacts: [
+              {contact: 'phone', content: '13812345678'},
+              {contact: 'qq', content: '12345678'},
+            ],
           }
         }
       }
     }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 #editor {
     background: #fff;
     box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
     display: flex;
     flex-direction: row;
+    overflow: auto;
     > nav {
         position: relative;
-        width: 80px;
+        width: 60px;
         background: #363636;
         color: white;
         .icon {
@@ -70,6 +101,7 @@
         }
         > ol {
             >li {
+                position: relative;
                 height: 48px;
                 display: flex;
                 justify-content: center;
@@ -77,7 +109,18 @@
                 &.active {
                     color: #4cc036;
                 }
-
+                &.active .gk_gl {
+                    border-color: transparent;
+                    border-right-color: #fff;
+                    border-style: solid;
+                    border-width: 8.5px 8.5px 8.5px 0;
+                    position: absolute;
+                    right: -1px;
+                    top: 5px;
+                    z-index: 1;
+                    height: 0;
+                    width: 0;
+                }
             }
         }
         .seting {
@@ -88,5 +131,34 @@
 
         }
     }
+    .panels {
+        flex-grow: 1;
+        >li {
+            padding: 18px;
+            h3 {
+                text-align: center;
+                margin-bottom: 20px;
+            }
+            .resumeField {
+                >lable {
+                    display: block;
+                }
+                input[type=text]{
+                    margin: 16px 0;
+                    border: 1px solid #ddd;
+                    box-shadow : inset 0 1px 3px 0 rgba(0,0,0,0.25);
+                    width: 100%;
+                    height: 40px;
+                    padding: 0 8px;
+                }
+            }
+            hr {
+                border: none;
+                border-top: 2px solid #4cc036;
+                margin: 24px 0;
+            }
+        }
+    }
+
 }
 </style>
